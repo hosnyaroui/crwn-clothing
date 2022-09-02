7e9f2fbfc5fcd42767c5645e89c48b4b783cd698
Husny Aroui <devilishcoder@gmail.com>
2022-09-02 21:06:43 +0100

    added router styling navbar and link pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.comonent.jsx';
 
-function App() {
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
+const App = () => {
   return (
-    <div>
-     <HomePage/>
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
+
